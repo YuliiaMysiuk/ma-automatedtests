2bf47fe2d2f5623c7ed0cd1a25ba7e8455f76105
Julie Mysiuk <yuliamysiuk@Yulias-MacBook-Pro.local>
2017-09-12 23:54:00 +0300

    Added TestBase. For real now.

---
+++ Sample_1/src/test/java/com/yuliia/macademy/tests/TestBase.java
@@ -0,0 +1,43 @@
+package com.yuliia.macademy.tests;
+
+import com.yuliia.macademy.pages.LoginPage;
+import com.yuliia.macademy.pages.MainPage;
+import com.yuliia.macademy.pages.MyCoursesPage;
+import com.yuliia.macademy.pages.ProfilePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    protected WebDriver driver;
+    protected String mainAcademyURL = "http://lms.mainacad.com/";
+    protected String userNameInput = "iuliia_mysiuk";
+    protected String userPasswordInput = "Iuliia_Mysiuk06";
+    protected String userProfileURL = "http://lms.mainacad.com/user/profile.php?id=3563";
+
+    @BeforeSuite
+    public void initDriver() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.navigate().to(mainAcademyURL);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.manage().deleteAllCookies();
+    }
+}
